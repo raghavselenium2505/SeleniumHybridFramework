da2540b69d87df2568c132a88b5223b76a99ecea
raghavselenium2505 <raghavendra.2119818@gmail.com>
2023-04-25 13:07:43 +0530

    Commited code newly updated

---
+++ src/test/java/com/testcase/testsuite/NaukriUpdate.java
@@ -0,0 +1,34 @@
+package com.testcase.testsuite;
+
+import org.testng.annotations.Test;
+
+import com.base.web.TestBase;
+import com.relevantcodes.extentreports.LogStatus;
+import com.testcase.Pages.NaukriLoginPage;
+
+public class NaukriUpdate extends TestBase{
+	static int var;
+	@Test
+	public void UserCreation() throws Exception {
+		// test=report.startTest(getData("TC001", "Jira_Story_Name", xlsname));
+		test = report.startTest(getData("User", "Jira_Story_Name", xlsname));
+
+		String skip = getData("DashBoard", "User", xlsname);
+//		UserListPage user = new UserListPage(driver);
+		try {
+			if (skip.equalsIgnoreCase("N")) {
+				executioncount++;
+				test.log(LogStatus.INFO, getData("User", "Jira_Story_Description", xlsname));
+
+				NaukriLoginPage login1 = new NaukriLoginPage(driver);
+				test.log(LogStatus.INFO, "Enter the login the details");
+				login1.login();
+				Thread.sleep(5000);
+			}
+		}
+			catch(Exception e)
+			{
+				
+			}
+	}
+}
