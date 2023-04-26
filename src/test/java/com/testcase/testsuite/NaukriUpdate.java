dfb26e2ed0a678b861b43e1d4cafc933e9c6f97c
raghavselenium2505 <raghavendra.2119818@gmail.com>
2023-04-26 11:44:43 +0530

    Commited for futher runs

--- src/test/java/com/testcase/testsuite/NaukriUpdate.java
+++ src/test/java/com/testcase/testsuite/NaukriUpdate.java
@@ -1,13 +1,16 @@
 package com.testcase.testsuite;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import com.base.web.TestBase;
 import com.relevantcodes.extentreports.LogStatus;
+import com.testcase.Pages.NaukriHomePage;
 import com.testcase.Pages.NaukriLoginPage;
 
-public class NaukriUpdate extends TestBase{
+public class NaukriUpdate extends TestBase {
 	static int var;
+
 	@Test
 	public void UserCreation() throws Exception {
 		// test=report.startTest(getData("TC001", "Jira_Story_Name", xlsname));
@@ -21,12 +24,30 @@ public class NaukriUpdate extends TestBase{
 
 				NaukriLoginPage login1 = new NaukriLoginPage(driver);
 				test.log(LogStatus.INFO, "Enter the login the details");
-				login1.login();
+				login1.login(config.getProperty("userName"), config.getProperty("passWord"));
+				waitforelement(5000);
+				NaukriHomePage homepage = new NaukriHomePage(driver);
+				actionclick(driver.findElement(homepage.profileClick), "able to click on profileclick ",
+						"Unable to click on profileclick ");
+				waitforelement(2000);
+				click(homepage.viewprofile, "Able to click on viewprofile", "Unable to click on viewprofile");
+
+				waitforelement(4000);
+				actionclick(driver.findElement(homepage.profilesummary), "able to click on profilesummaryclick ",
+						"Unable to click on profilesummaryclick ");
+				waitforelement(5000);
+				String value = driver.findElement(homepage.profileText).getAttribute("value");
+				clear(homepage.profileText);
+				waitforelement(3000);
+				sendkeys(homepage.profileText, value, "Able to send the value for profilesummaryclick",
+						"Unable to send the profilesummaryclick");
+				waitforelement(3000);
+				click(homepage.saveButton, "Able to click on save button", "Unable to click on save button");
+				waitforelement(5000);
+				homepage.isdisplayed(homepage.texttoday, "Able to update", "Not updated");
 			}
+		} catch (Exception e) {
+
 		}
-			catch(Exception e)
-			{
-				
-			}
 	}
 }
