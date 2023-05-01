414181b5850d0f2e0c6278f59d6c31a8ed9a7506
raghavselenium2505 <raghavendra.2119818@gmail.com>
2023-05-01 17:46:51 +0530

    Commited with excel files#raghav

--- src/test/java/com/testcase/testsuite/SignupForMagento.java
+++ src/test/java/com/testcase/testsuite/SignupForMagento.java
@@ -16,9 +16,9 @@ public class SignupForMagento extends TestBase {
 		@Test
 		public void UserCreation() throws Exception {
 			// test=report.startTest(getData("TC001", "Jira_Story_Name", xlsname));
-			test = report.startTest(getData("User", "Jira_Story_Name", xlsname));
+			test = report.startTest(getData("Signup", "Jira_Story_Name", xlsname));
 
-			String skip = getData("DashBoard", "User", xlsname);
+			String skip = getData("DashBoard", "Signup", xlsname);
 			try {
 				if (skip.equalsIgnoreCase("N")) {
 					test.log(LogStatus.INFO, "Click on Create an Account");
@@ -27,7 +27,9 @@ public class SignupForMagento extends TestBase {
 
 					click(login.buttonCreateAnAccount, "Able to click on  Create button", "Unable to click on Create button");
 					waitforelement(5000);
-					login.signup("A", "B", "A@CD121.com", "CBA15487", "CBA15487");
+					login.signup(getData("Signup", "textFirstName", xlsname), getData("Signup", "textLastName", xlsname), getData("Signup", "textEmailId", xlsname), getData("Signup", "textPassword", xlsname), getData("Signup", "textConfirmPassword", xlsname));
+					//login.signup("A", "B", "A@CD121.com", "CBA15487", "CBA15487");
+					
 					click(login.buttonRegister, "Able to click  on Register", "Unable to click on Register");
 				if (driver.findElement(By.xpath("//h1[text()='My Dashboard']")).isDisplayed()) {
 					test.log(LogStatus.PASS, "Enter all valid details for signup");
