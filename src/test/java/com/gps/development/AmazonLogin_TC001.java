d6ccd988cd29d81bfc423584338f7b8ef16be1e3
raghavselenium2505 <raghavendra.2119818@gmail.com>
2026-02-26 09:38:53 +0530

    commited code#raghav

--- src/test/java/com/gps/development/AmazonLogin_TC001.java
+++ src/test/java/com/gps/development/AmazonLogin_TC001.java
@@ -1,101 +1,90 @@
 package com.gps.development;
 
-import java.io.FileReader;
 import java.time.Duration;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-import com.web.utilities.*;
-import com.aventstack.extentreports.Status;
 import com.gps.base.TestBase;
 import com.gps.pages.AmazonHomePage;
 import com.gps.pages.AmazonLoginPage;
+import com.gps.utilities.DynamicDataProvider;
 
 public class AmazonLogin_TC001 extends TestBase {
 
-    private final String xlsname = "Gps_Rules.xls";
-    private final JSONParser parser = new JSONParser();
+    // 20 TESTS LIKE THIS
+    @Test(groups = {"json"},
+          dataProvider = "dynamicData",
+          dataProviderClass =
+                  DynamicDataProvider.class)
+    public void TC_Login_JSON(
+            String username,
+            String password,
+            String searchItem) {
 
-    @Test(retryAnalyzer = com.web.utilities.AIRetryAnalyzer.class)
-    public void Amazon_Login_TC001() {
+        System.out.println(
+                "Running JSON Test");
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(searchItem);
+        
+        AmazonHomePage home = new AmazonHomePage();
 
-        try {
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
 
-            System.out.println("Login to Amazon");
+        click(
+                home.linkAccount,
+                "Able to click on link account",
+                "Unable to click on link account"
+        );
+        
 
-            AmazonHomePage home = new AmazonHomePage();
+        AmazonLoginPage login = new AmazonLoginPage(getDriver());
 
-            getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+        login.login(
+                config.getProperty("userName"),
+                config.getProperty("passWord"),
+                "Able to login",
+                "Unable to login"
+        );
 
-            click(
-                    home.linkAccount,
-                    "Able to click on link account",
-                    "Unable to click on link account"
-            );
-            
-
-            AmazonLoginPage login = new AmazonLoginPage(getDriver());
-
-            login.login(
-                    config.getProperty("userName"),
-                    config.getProperty("passWord"),
-                    "Able to login",
-                    "Unable to login"
-            );
-
-            getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
-
-            String s = "N";
-
-            if (s.equalsIgnoreCase("Y")) {
-
-                Object obj = parser.parse(
-                        new FileReader("C:\\Users\\RaghavendraD\\Desktop\\sample.json"));
-
-                JSONObject jsonObject = (JSONObject) obj;
-                String name = (String) jsonObject.get("name");
-
-                getDriver()
-                        .findElement(By.id("twotabsearchtextbox"))
-                        .sendKeys(name);
-
-            } else if (s.equalsIgnoreCase("N")) {
-
-                getDriver()
-                        .findElement(By.id("twotabsearchtextbox"))
-                        .sendKeys(
-                                getDataFromExcel("Signin", "Jira_Story_Name", xlsname)
-                        );
-
-            } else {
-                System.out.println("Please configure datasheet option");
-            }
-
-            Thread.sleep(6000);
-
-        } catch (Exception e) {
-
-            System.out.println("❌ Test Failed: " + e.getMessage());
-
-            // 🔥 AI Analysis
-            //String aiSuggestion = AITestAnalyzer.analyze(e);
-         //   String screenshotPath = screenshotutil.captureScreenshot(getDriver());
-
-           // test.get().log(
-			/*
-			 * Status.FAIL, aiSuggestion + "<br>" + "<a href='" + screenshotPath +
-			 * "' target='_blank'>" + "Click here for screenshot" + "</a>" );
-			 */
-
-            throw new RuntimeException(e);
-
-        }
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
     }
 
-    private String getDataFromExcel(String sheetName, String columnName, String xls) {
-        return excelutil.getData(sheetName, columnName, xls);
+    // 80 TESTS LIKE THIS
+    @Test(groups = {"excel"},
+          dataProvider = "dynamicData",
+          dataProviderClass =
+                  DynamicDataProvider.class)
+    public void TC_Login_Excel(
+            String username,
+            String password,
+            String searchItem) {
+
+        System.out.println(
+                "Running Excel Test");
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(searchItem);
+        AmazonHomePage home = new AmazonHomePage();
+
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+
+        click(
+                home.linkAccount,
+                "Able to click on link account",
+                "Unable to click on link account"
+        );
+        
+
+        AmazonLoginPage login = new AmazonLoginPage(getDriver());
+
+        login.login(
+                config.getProperty("userName"),
+                config.getProperty("passWord"),
+                "Able to login",
+                "Unable to login"
+        );
+
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
     }
 }
--- src/test/java/com/gps/utilities/ExcelUtil.java
+++ src/test/java/com/gps/utilities/ExcelUtil.java
@@ -8,31 +8,120 @@ import org.apache.poi.ss.usermodel.DataFormatter;
 
 import com.gps.base.TestBase;
 
-public class ExcelUtil extends TestBase{
+public class ExcelUtil extends TestBase {
 
-	public String getData(String SheetName, String ColName, String excelName) {
-		String returnValue = "";
-		try {
-			FileInputStream fis = new FileInputStream(
-					System.getProperty("user.dir") + "\\src\\test\\resources\\excel\\" + excelName);
-			HSSFWorkbook wb = new HSSFWorkbook(fis);
-			HSSFSheet sh = wb.getSheet(SheetName);
-			int rowCount = sh.getLastRowNum();
-			DataFormatter formatter = new DataFormatter(); // ✅ Use formatter to avoid type mismatch
+    // ---------------------------------------------
+    // OLD METHOD (DO NOT REMOVE)
+    // ---------------------------------------------
+    public String getData(String SheetName,
+                          String ColName,
+                          String excelName) {
 
-			for (int i = 0; i <= rowCount; i++) {
-				String val = formatter.formatCellValue(sh.getRow(i).getCell(0)); // updated to use formatter
-				if (val.equalsIgnoreCase(ColName)) {
-					returnValue = formatter.formatCellValue(sh.getRow(i).getCell(1)); // updated to use formatter
-					System.out.println(returnValue);
-					break;
-				}
-			}
-			//wb.close();
-			fis.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return returnValue;
-	}
+        String returnValue = "";
+
+        try {
+
+            FileInputStream fis =
+                    new FileInputStream(
+                            System.getProperty("user.dir")
+                                    + "\\src\\test\\resources\\excel\\"
+                                    + excelName);
+
+            HSSFWorkbook wb = new HSSFWorkbook(fis);
+            HSSFSheet sh = wb.getSheet(SheetName);
+
+            int rowCount = sh.getLastRowNum();
+            DataFormatter formatter =
+                    new DataFormatter();
+
+            for (int i = 0; i <= rowCount; i++) {
+
+                String val =
+                        formatter.formatCellValue(
+                                sh.getRow(i).getCell(0));
+
+                if (val.equalsIgnoreCase(ColName)) {
+
+                    returnValue =
+                            formatter.formatCellValue(
+                                    sh.getRow(i).getCell(1));
+                    break;
+                }
+            }
+
+            wb.close();
+            fis.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return returnValue;
+    }
+
+    // ---------------------------------------------
+    // NEW METHOD FOR DATAPROVIDER
+    // ---------------------------------------------
+    public Object[][] getSheetDataByTestCaseName(
+            String sheetName,
+            String testCaseName,
+            String excelName) {
+
+        try {
+
+            FileInputStream fis =
+                    new FileInputStream(
+                            System.getProperty("user.dir")
+                                    + "\\src\\test\\resources\\excel\\"
+                                    + excelName);
+
+            HSSFWorkbook wb = new HSSFWorkbook(fis);
+            HSSFSheet sh = wb.getSheet(sheetName);
+
+            int rowCount = sh.getLastRowNum();
+            int colCount =
+                    sh.getRow(0).getLastCellNum();
+
+            DataFormatter formatter =
+                    new DataFormatter();
+
+            for (int i = 1; i <= rowCount; i++) {
+
+                String currentTestName =
+                        formatter.formatCellValue(
+                                sh.getRow(i).getCell(0));
+
+                if (currentTestName
+                        .equalsIgnoreCase(testCaseName)) {
+
+                    Object[][] data =
+                            new Object[1][colCount - 1];
+
+                    for (int j = 1;
+                         j < colCount;
+                         j++) {
+
+                        data[0][j - 1] =
+                                formatter.formatCellValue(
+                                        sh.getRow(i).getCell(j));
+                    }
+
+                    wb.close();
+                    fis.close();
+
+                    return data;
+                }
+            }
+
+            wb.close();
+            fis.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        throw new RuntimeException(
+                "Test case not found in Excel: "
+                        + testCaseName);
+    }
 }
